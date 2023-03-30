787222e32c6d2b81fb44d00836672a5bdbafe963
MakerDZ <mcskb91@gmail.com>
2023-03-31 01:21:00 +0630

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
